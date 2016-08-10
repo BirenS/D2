c2caa58b6119074f97b2694597acfdd35250b50d
BirenS <soni.biren@gmail.com>
2016-08-10 16:14:21 +0530

    Application is created

---
+++ GreetingsWebApp/GreetingsWebApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GreetingsWebApp.Startup))]
+namespace GreetingsWebApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
