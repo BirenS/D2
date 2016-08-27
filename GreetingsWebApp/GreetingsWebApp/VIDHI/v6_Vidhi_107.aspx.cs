a56986ee54709dbbd388114abfb29411424c99a1
vidhi28 <vidhi28@users.noreply.github.com>
2016-08-27 09:40:11 +0530

    Greeting card with Cookie Technique

---
+++ GreetingsWebApp/GreetingsWebApp/VIDHI/v6_Vidhi_107.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.Drawing.Text;
+using System.ComponentModel;
+using System.Net; 
+
+
+namespace WebApplication5
+{
+    public partial class v6_Vidhi_107 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                // Set color options. 
+                string[] colorArray = Enum.GetNames(typeof(KnownColor));
+                lstBackColor.DataSource = colorArray;
+                lstBackColor.DataBind();
+
+                // Set font options.  
+                InstalledFontCollection fonts = new InstalledFontCollection();
+                foreach (FontFamily family in fonts.Families)
+                {
+                    lstFontName.Items.Add(family.Name);
+                }
+
+                // Set border style options by adding a series of     
+                // ListItem objects.         
+                ListItem item = new ListItem();
+                string[] borderStyleArray = Enum.GetNames(typeof(BorderStyle));
+                lstBorder.DataSource = borderStyleArray;
+                lstBorder.DataBind();
+
+                // Select the ﬁrst border option.       
+                lstBorder.SelectedIndex = 0;
+                // Set the picture.          
+            }
+        }
+
+        protected void cmdUpdate_Click(object sender, EventArgs e)
+        {
+            // Create the Request.QueryString object. 
+            HttpCookie cookie = new HttpCookie("Greetings");
+            cookie["color"] = lstBackColor.SelectedItem.Text;
+            cookie["fname"] = lstFontName.SelectedItem.Text;
+            cookie["fsize"] = txtFontSize.Text;
+            cookie["border"] = lstBorder.SelectedItem.Text;
+            cookie["pic"] =  chkPicture.Checked.ToString();
+            cookie["text"] = txtGreeting.Text;
+            Response.Cookies.Add(cookie);
+            Response.Redirect("v6_redirected_107.aspx");
+        }
+        protected void ControlChanged(object sender, System.EventArgs e)
+        {    // Refresh the greeting card (because a control was changed). 
+            UpdateCard();
+        }
+
+        private void UpdateCard()
+        {    // (The code that draws the greeting card goes here.) 
+        } 
+       
+    }
+}
---
+++ GreetingsWebApp/GreetingsWebApp/VIDHI/v6_redirected_107.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.Drawing.Text;
+using System.ComponentModel;
+
+namespace WebApplication5
+{
+    public partial class v6_redirected_107 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HttpCookie cookie = Request.Cookies["Greetings"];
+            if (cookie != null)
+            {
+                // Update the color.  
+                pnlCard.BackColor = Color.FromName(cookie["color"]);
+                // Update the font.        
+                lblGreeting.Font.Name = cookie["fname"];
+                if (Int32.Parse(cookie["fsize"]) > 0)
+                {
+                    lblGreeting.Font.Size = FontUnit.Point(Int32.Parse(cookie["fsize"]));
+                }
+
+                // Find the appropriate TypeConverter for the BorderStyle enumeration.
+                TypeConverter converter = TypeDescriptor.GetConverter(typeof(BorderStyle));
+                // Update the border style using the value from the converter.
+                pnlCard.BorderStyle = (BorderStyle)converter.ConvertFromString(cookie["border"]);
+                // int borderValue = Int32.Parse(lstBorder.SelectedItem.Value);  
+                // pnlCard.BorderStyle = (BorderStyle)borderValue;       
+                // Update the picture.
+                imgDefault.ImageUrl = "imgDefault.jpeg";
+                if (cookie["pic"] == "True")
+                {
+                    imgDefault.Visible = true;
+                }
+                else
+                {
+                    imgDefault.Visible = false;
+                }
+                // Set the text.     
+                lblGreeting.Text = cookie["text"];
+            }
+            UpdateCard();
+        }
+        protected void ControlChanged(object sender, System.EventArgs e)
+        {    // Refresh the greeting card (because a control was changed). 
+            UpdateCard();
+        }
+
+        private void UpdateCard()
+        {
+            // (The code that draws the greeting card goes here.) 
+        }
+    }
+}
