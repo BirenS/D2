5d948d67c5139c7f9e20d4f86381c3ecfaa92afa
jaimishpatel5 <jaimishpatel5@gmail.com>
2016-09-20 20:21:11 +0530

    v9 session version is completed.

---
+++ GreetingsWebApp/GreetingsWebApp/jaimish_91/version9.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.Drawing.Text;
+using System.ComponentModel;
+using System.Net;
+
+namespace GreetingsWebApp.jaimish_91
+{
+    public partial class version9 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            InstalledFontCollection fonts = new InstalledFontCollection();
+            if (!this.IsPostBack)
+            {
+                // Set color options.
+                string[] colorArray = Enum.GetNames(typeof(KnownColor));
+                lstBackColor.DataSource = colorArray;
+                lstBackColor.DataBind();
+                // Set font options.
+                foreach (FontFamily family in fonts.Families)
+                {
+                    lstFontName.Items.Add(family.Name);
+                }
+
+                // Set border style options
+                string[] borderStyleArray = Enum.GetNames(typeof(BorderStyle));
+                lstBorder.DataSource = borderStyleArray;
+                lstBorder.DataBind();
+                // Select the first border option.
+                lstBorder.SelectedIndex = 0;
+                // Set the picture.
+
+            }
+        }
+        protected void cmdUpdate_Click(object sender, EventArgs e)
+        {
+            Session["backcolor"] = lstBackColor.SelectedItem.Text;
+            Session["fontname"] = lstFontName.SelectedItem.Text;
+            Session["fontsize"] = txtFontSize.Text;
+            Session["borderstyle"] = lstBorder.SelectedItem.Text;
+            Session["greetingtext"] = txtGreeting.Text;
+            Session["chkpic"] = chkPicture.Checked;
+            //   if(!Session.IsNewSession)
+            Response.Redirect("version91.aspx");
+        }
+    }
+}
---
+++ GreetingsWebApp/GreetingsWebApp/jaimish_91/version91.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.Drawing.Text;
+using System.ComponentModel;
+using System.Net;
+
+namespace GreetingsWebApp.jaimish_91
+{
+    public partial class version91 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            imgDefault.ImageUrl = "images/dafault.jpg";
+            pnlCard.BackColor = Color.FromName(Session["backcolor"].ToString());
+
+            // Update the font.
+            lblGreeting.Font.Name = Session["fontname"].ToString();
+            if (Int32.Parse(Session["fontsize"].ToString()) > 0)
+            {
+                lblGreeting.Font.Size =
+                FontUnit.Point(Int32.Parse(Session["fontsize"].ToString()));
+            }
+            // Update the border style. This requires two conversion steps.
+            // First, the value of the list item is converted from a string
+            // into an integer. Next, the integer is converted to a value in
+            // the BorderStyle enumeration.
+            // Find the appropriate TypeConverter for the BorderStyle enumeration.
+            TypeConverter converter =
+            TypeDescriptor.GetConverter(typeof(BorderStyle));
+            // Update the border style using the value from the converter.
+            pnlCard.BorderStyle = (BorderStyle)converter.ConvertFromString(
+            Session["borderstyle"].ToString());
+            // Update the picture.
+            if (Session["chkpic"].ToString() == "True")
+            {
+                imgDefault.Visible = true;
+            }
+            else
+            {
+                imgDefault.Visible = false;
+            }
+            // Set the text.
+            lblGreeting.Text = Session["greetingtext"].ToString();
+
+            //Session.Abandon();
+        }
+    }
+}
